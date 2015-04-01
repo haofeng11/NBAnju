f90f424fc80bbcc0f8d7812cb77b1a1e2f2fcd81
qinzhenning <1026529451@qq.com>
2015-04-01 14:39:19 +0800

    搭建了springmvc+spring+hibernate框架，并新建一个user对象来测试

---
+++ src/main/java/edu/nju/nba/service/IUserService.java
@@ -0,0 +1,10 @@
+package edu.nju.nba.service;
+
+import edu.nju.nba.bean.User;
+
+public interface IUserService {
+	
+	public boolean registe(User user);
+
+
+}
---
+++ src/main/java/edu/nju/nba/service/impl/UserService.java
@@ -0,0 +1,34 @@
+package edu.nju.nba.service.impl;
+
+
+
+
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.nju.nba.bean.User;
+import edu.nju.nba.dao.IGeneralDao;
+import edu.nju.nba.service.IUserService;
+
+@Service
+public class UserService implements IUserService {
+	
+	@Autowired
+	private IGeneralDao generalDao;
+	
+	/* 
+	 * 这里要有事务注解，默认readOnly=true,不设置的话会报错。
+	 * insert和update操作都要。
+	 */
+	
+	@Transactional(readOnly=false)
+	public boolean registe(User user) {
+		generalDao.save(user);
+		return false;
+	}
+
+}
